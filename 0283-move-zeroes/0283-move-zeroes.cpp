35da4f31bc173251591af64148c81cb281c20e7d
Mahi Joshi <jmahi0432@gmail.com>
2026-08-11 22:24:38 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int nonzeroes=0;
@@ -7,6 +7,25 @@ public:
                 swap(nums[i] , nums[nonzeroes]);
                 nonzeroes++;
             }
+        }
+    }
+};*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int writeIndex = 0;
+
+        for (int readIndex = 0; readIndex < nums.size(); readIndex++) {
+            if (nums[readIndex] != 0) {
+                nums[writeIndex] = nums[readIndex];
+                writeIndex++;
+            }
+        }
+
+        while (writeIndex < nums.size()) {
+            nums[writeIndex] = 0;
+            writeIndex++;
         }
     }
 };
